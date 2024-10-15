905e731a09d8ec281e8c7c5b7c231603fc386a24
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2024-10-15 09:08:56 +0100

    feat(DataSegregator): Created a class and method for segregating the data with a characteristic.

---
+++ src/net/salesianos/DataSegregator.java
@@ -0,0 +1,35 @@
+package net.salesianos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DataSegregator {
+  public static Map<String, Set<String>> segregatePopulationByCharacteristic(String[] characteristics,
+      ArrayList<String> dataArrayList,
+      String characteristic) {
+    Map<String, Set<String>> segregatedPopulation = new HashMap<>();
+    String[] data = new String[dataArrayList.size()];
+    dataArrayList.toArray(data);
+    int characteristicIndex = 0;
+    data[0].indexOf(characteristic, 0);
+    String[] personCharacteristics = data[0].split(",");
+    for (int i = 0; i < personCharacteristics.length; i++) {
+      if (personCharacteristics[i].equals(characteristic)) {
+        characteristicIndex = i;
+      }
+    }
+    for (String person : data) {
+      String[] splitedPerson = person.split(",");
+      if (!splitedPerson[characteristicIndex].equals(characteristic)) {
+        System.out.println(splitedPerson[characteristicIndex]);
+        Set<String> people = segregatedPopulation.computeIfAbsent(splitedPerson[characteristicIndex],
+            k -> new HashSet<>());
+        people.add(person);
+      }
+    }
+    return segregatedPopulation;
+  }
+}
