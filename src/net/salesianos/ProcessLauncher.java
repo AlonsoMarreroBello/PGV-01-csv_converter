3a8be3574f8f0fe37e68636ac23bc9f91641fd00
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-10-15 10:36:38 +0200

    feat(ProcessLauncher): Created a class and method for launching a process.

---
+++ src/net/salesianos/ProcessLauncher.java
@@ -0,0 +1,16 @@
+package net.salesianos;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ProcessLauncher {
+
+    public static Process startProccess(String route, String data, String output, String errorRoute)
+            throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder("java",
+                route, data, output);
+        processBuilder.redirectOutput(new File(output));
+        processBuilder.redirectError(new File(errorRoute));
+        return processBuilder.start();
+    }
+}
