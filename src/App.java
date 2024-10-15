616786d5b547f9167ddeae5b2c4e2a26b8293161
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-10-15 13:52:40 +0200

    feat(App): Created the switch for the main program.

--- src/App.java
+++ src/App.java
@@ -1,41 +1,66 @@
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 import net.salesianos.DataExtractor;
 import net.salesianos.DataSegregator;
+import net.salesianos.utils.Menu;
 import net.salesianos.utils.ProcessLauncher;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    String csvRoute = "data\\input\\data.csv";
-    String outputRoute = "data\\output\\";
     final String ERROR_ROUTE = "logs\\";
-    String[] characteristics = { "Name", "Nationality", "Age", "Profession" };
-    String characteristic = "Name";
+    Scanner SC = new Scanner(System.in);
+    Menu.showMenu();
+    switch (SC.nextLine()) {
+      case "1":
+        System.out.println("Introduzca la ruta del CSV: ");
+        String csvRoute = SC.nextLine();
+        System.out.println("Introduzca la ruta de salida:");
+        String outputRoute = SC.nextLine();
+        String[] characteristics = { "Name", "Nationality", "Age", "Profession" };
+        String characteristic = "";
+        boolean keepAsking = true;
+        while (keepAsking) {
+          System.out.println("Introduzca la caracteristica por la cual separar: \nName, Nationality, Age o Profession");
+          characteristic = SC.nextLine();
+          for (int i = 0; i < characteristics.length; i++) {
+            if (characteristics[i].contains(characteristic)) {
+              keepAsking = false;
+            }
+          }
+        }
+        ArrayList<String> list = DataExtractor.getDataFromCSV(csvRoute);
+        Map<String, Set<String>> segregatedData = DataSegregator.segregatePopulationByCharacteristic(
+            characteristics, list, characteristic);
+        ArrayList<Process> processes = new ArrayList<>();
+        for (String string : segregatedData.keySet()) {
+          if (string.trim() != characteristic) {
+            String data = "";
+            for (String charasteristic : characteristics) {
+              data += charasteristic + ",";
+            }
+            for (String setData : segregatedData.get(string)) {
+              data += "\n" + setData;
+            }
+            String filename = string.replaceAll(" ", "");
+            Process process = ProcessLauncher.startProcess("src\\net\\salesianos\\DataSaver.java", data,
+                outputRoute + filename + ".txt",
+                ERROR_ROUTE + filename + "_err.log");
+            processes.add(process);
+          }
+        }
+        for (Process process : processes) {
+          process.waitFor();
+        }
+        break;
+      case "2":
 
-    ArrayList<String> list = DataExtractor.getDataFromCSV(csvRoute);
-    Map<String, Set<String>> segregatedData = DataSegregator.segregatePopulationByCharacteristic(
-        characteristics, list, characteristic);
-    ArrayList<Process> processes = new ArrayList<>();
-    for (String string : segregatedData.keySet()) {
-      if (string.trim() != characteristic) {
-        String data = "";
-        for (String charasteristic : characteristics) {
-          data += charasteristic + ",";
-        }
-        for (String setData : segregatedData.get(string)) {
-          data += "\n" + setData;
-        }
-        String filename = string.replaceAll(" ", "");
-        Process process = ProcessLauncher.startProcess("src\\net\\salesianos\\DataSaver.java", data,
-            outputRoute + filename + ".txt",
-            ERROR_ROUTE + filename + "_err.log");
-        processes.add(process);
-      }
+        break;
+      default:
+        break;
     }
-    for (Process process : processes) {
-      process.waitFor();
-    }
+    SC.close();
   }
 }
